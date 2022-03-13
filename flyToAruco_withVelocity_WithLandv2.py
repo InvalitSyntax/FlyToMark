3abed70e5773e57168d02fcb13360fb6dacc5b1f
InvalitSyntax <81087114+InvalitSyntax@users.noreply.github.com>
2022-03-13 23:02:20 +0700

    Rewrite code, fix dinamic P

--- flyToAruco_withVelocity_WithLandv2.py
+++ flyToAruco_withVelocity_WithLandv2.py
@@ -32,61 +32,60 @@ def land_wait():
     while get_telemetry().armed:
         rospy.sleep(0.2)
 
-
 zFlight = 1
 lastPoseOfAruco = [0, 0]
-
-def followAruco(ID=10, VZ=-0.3):
-    print()
-    # take msg
-    msg = rospy.wait_for_message('/aruco_detect/markers', MarkerArray)
-
-    # take only needble marker
-    markers = msg.markers
-    dataAruco = list(filter(lambda x: x.id == ID, markers))
-
-    # fix floating height
-    telemetry = get_telemetry(frame_id='aruco_map')
-
-    P = 0.6 if telemetry.y > 1 else 1
-    zVelocity = zFlight - telemetry.z
-
-
-    if len(dataAruco) != 0:
-        # pose of marker
-        dataAruco = dataAruco[0]
-        poseAruco = dataAruco.pose.position
-
-        print(f'x: {poseAruco.x}')
-        print(f'y: {poseAruco.y}')
-        lastPoseOfAruco[:] = [poseAruco.y + 0.07, poseAruco.x]
-
-        # +0.07 - поправка на камеру (она не в центре дрона - на 7 сантиметра дальше по y)
-        xVelocity = ((poseAruco.y + 0.07) * P)
-        yVelocity = (poseAruco.x * P)
-        if poseAruco.y < 0.15 and poseAruco.x < 0.15:
-            set_velocity(vx=xVelocity, vy=yVelocity, vz=VZ, frame_id='body')
-
-        else:
-            set_velocity(vx=xVelocity, vy=yVelocity, vz=0, frame_id='body')
-    else:
-        print('nan')
-        #set_velocity(vx=0, vy=0, yaw=float('nan'), vz=zVelocity, frame_id='aruco_map')
-        return 'lost'
+ID = 90
+VZ = -0.3
 
 navigate_wait(z=1, frame_id='body', yaw=float('nan'), auto_arm=True)
-navigate_wait(x=2, y=2, z=zFlight, yaw=math.pi / 2, frame_id='aruco_map')
+navigate_wait(x=1, y=1, z=zFlight, yaw=math.pi / 2, frame_id='aruco_map')
 
 
 def logicOfFlight():
+    telemetry = get_telemetry(frame_id='aruco_map')
+    xStart = telemetry.x
+    yStart = telemetry.y
+
     while not rospy.is_shutdown():
         telemetry = get_telemetry(frame_id='aruco_map')
 
-        xStart = telemetry.x
-        yStart = telemetry.y
-        zStart = telemetry.z
+        msg = rospy.wait_for_message('/aruco_detect/markers', MarkerArray)
+        # take only needble marker
+        markers = msg.markers
+        dataAruco = list(filter(lambda x: x.id == ID, markers))
 
-        while followAruco() == 'lost':
+
+        # if find mark:
+        if len(dataAruco) != 0:
+            # pose of marker
+            dataAruco = dataAruco[0]
+            poseAruco = dataAruco.pose.position
+
+            print(f'x: {poseAruco.x}')
+            print(f'y: {poseAruco.y}')
+            lastPoseOfAruco[:] = [(poseAruco.y + 0.07), (poseAruco.x)]
+
+            # +0.07 - поправка на камеру (она не в центре дрона - на 7 сантиметра дальше по y)
+            if telemetry.z < 1 or (abs(poseAruco.y) < 1 and abs(poseAruco.x) < 1):
+                P = 1
+            else:
+                P = 0.6
+            print(f'P = {P}')
+            xVelocity = ((poseAruco.y + 0.07) * P)
+            yVelocity = (poseAruco.x * P)
+
+            if abs(poseAruco.y) < 0.15 and abs(poseAruco.x) < 0.15:
+                set_velocity(vx=xVelocity, vy=yVelocity, vz=VZ, frame_id='body')
+
+            else:
+                set_velocity(vx=xVelocity, vy=yVelocity, vz=0, frame_id='body')
+
+            xStart = telemetry.x
+            yStart = telemetry.y
+        
+        # if don't find mark
+        else:
+            print('nan')
 
             # land
             if rospy.wait_for_message('rangefinder/range', Range).range < 0.3:
@@ -94,8 +93,6 @@ def logicOfFlight():
                 while rospy.wait_for_message('rangefinder/range', Range).range > 0.1:
                     pass
                 return
-
-
 
             telemetry = get_telemetry(frame_id='aruco_map')
             if 0.5 < telemetry.x < 3.5 and 0.5 < telemetry.y < 3.5 and telemetry.z < 1.5 and abs(telemetry.x - xStart) < 1 and abs(telemetry.y - yStart) < 1:
